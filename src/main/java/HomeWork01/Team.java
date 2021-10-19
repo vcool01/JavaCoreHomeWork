9d56bf8e8274c0d746bd17e14dd0248b4e1d55b1
vcool01 <v.cool-01@list.ru>
2021-10-19 14:23:22 +0300

    DZ1

---
+++ src/main/java/HomeWork01/Team.java
@@ -0,0 +1,42 @@
+package HomeWork01;
+
+public  class Team {
+    protected String teamName;
+    public int countMember = 4;
+    protected Player [] memberTeam = new Player[countMember];
+
+    public Team(String name){
+        this.teamName = name;
+        this.memberTeam[0] = new Player("Оля");
+        this.memberTeam[1] = new Player("Вася");
+        this.memberTeam[2] = new Player("Глеб");
+        this.memberTeam[3] = new Player("Борис");
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String name) {
+        this.teamName = name;
+    }
+
+    public void passedDistance(Course c){
+        System.out.println("Прошли дистанцию из команды " +
+                this.getTeamName() + ": ");
+        for(int i = 0; i < this.countMember; i++){
+
+            if (c.doIt(this.memberTeam[i])){
+            System.out.println(this.memberTeam[i].getName());
+//            System.out.println(team1.memberTeam[i].toString());
+        }
+    }
+    }
+
+    public void allMembers(){
+        System.out.println(this.getTeamName() + " :");
+        for(int i = 0; i < countMember; i++) {
+            System.out.println(this.memberTeam[i].toString());
+        }
+    }
+}
